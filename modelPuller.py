45cd469955fb61018aed7411b346fe34c5384d04
fahmi-g <83896608+fahmi-g@users.noreply.github.com>
2022-06-09 16:06:26 +0700

    Fixed large model & Udated model

--- modelPuller.py
+++ modelPuller.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://github.com/fahmi-g/Rayuan-Image-Classification-API/raw/main/model_experiment.h5"
+url = "https://github.com/fahmi-g/Rayuan-Image-Classification-API/raw/main/model_final.h5"
 response = requests.get(url)
 open("latest_model.h5", "wb").write(response.content)
 
